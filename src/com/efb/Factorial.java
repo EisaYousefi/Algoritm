b3982dc06a2a7dc3e100c76571d1869b731e3ed0
borwa <eisa.yousefi.2910@gmail.com>
2022-01-21 18:40:23 +0330

    14001101

--- src/com/efb/Factorial.java
+++ src/com/efb/Factorial.java
@@ -2,9 +2,9 @@ package com.efb;
 
 public class Factorial {
 
-    public long fact(long num){
-        if (num<1)
-            return 1;
-        return num*(fact(num-1));
+    public long fact(long num) {
+        if (num < 0) return -1;
+        if (num <= 1) return 1;
+        return num * (fact(num - 1));
     }
 }
--- src/com/efb/Fibonachi.java
+++ src/com/efb/Fibonachi.java
@@ -5,28 +5,27 @@ import java.util.List;
 
 public class Fibonachi {
 
-    public void exec(long num){
+    public void exec(long num) {
         List<Long> list = new ArrayList<>();
-        if (num<0) {
+        if (num < 0) {
             System.out.println("number > 0");
             return;
         }
-        if (num<2){
+        if (num <=1) {
             System.out.println(num);
             return;
         }
         list.add(1L);
         list.add(1L);
-        for (int i =1; i <=num ; i++) {
-                list.add(list.get(i-1)+list.get(i));
+        for (int i = 1; i <= num; i++) {
+            list.add(list.get(i - 1) + list.get(i));
         }
         System.out.println(list);
     }
 
-    public long  execBackFun(long num){
-        if (num<=2)
-            return 1;
-        else
-            return execBackFun(num-1)+execBackFun(num-2);
+    public long execBackFun(long num) {
+        if (num < 0) return -1;
+        if (num <= 1) return num;
+        return execBackFun(num - 1) + execBackFun(num - 2);
     }
 }
